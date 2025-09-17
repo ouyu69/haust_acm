66fb5d0074ddf6c44c28f12691ae72ce17d97647
ouyu69 <zhiyuanyang410@gmail.com>
2025-09-17 18:53:25 +0800

    添加的message变量，动态的修改正确请求的返回信息

--- src/main/java/com/haust_acm/controller/ABaseController.java
+++ src/main/java/com/haust_acm/controller/ABaseController.java
@@ -7,11 +7,11 @@ public class ABaseController {
     protected static final String STATUS_SUCCESS = "success" ;
     protected static final String STATUS_ERROR = "error" ;
 
-    protected <T> ResponseVO getSuccessResponseVO(T t) {
+    protected <T> ResponseVO getSuccessResponseVO(T t,String message) {
         ResponseVO<T> responseVO = new ResponseVO();
         responseVO.setStatus(STATUS_SUCCESS);
         responseVO.setCode(ResponseCodeEnum.CODE_200.getCode());
-        responseVO.setInfo(ResponseCodeEnum.CODE_200.getMsg());
+        responseVO.setInfo(message == null ? ResponseCodeEnum.CODE_200.getMsg():message);
         responseVO.setData(t);
         return responseVO;
     }

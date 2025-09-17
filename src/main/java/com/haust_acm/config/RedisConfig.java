5f523c3bd5176381455cd9c56964aa86c920e16f
ouyu69 <zhiyuanyang410@gmail.com>
2025-09-17 19:12:16 +0800

    redis配置类

---
+++ src/main/java/com/haust_acm/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.haust_acm.config;
+
+import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @FileName RedisConfig
+ * @Description
+ * @Author ouyu
+ * @Date 2025-09-17
+ **/
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Bean
+    @SuppressWarnings("all")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        StringRedisSerializer keySerializer = new StringRedisSerializer();
+        GenericFastJsonRedisSerializer valueSerializer = new GenericFastJsonRedisSerializer();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        // 序列化 key 和 value
+        redisTemplate.setKeySerializer(keySerializer);
+        redisTemplate.setValueSerializer(valueSerializer);
+        redisTemplate.setHashKeySerializer(keySerializer);
+        redisTemplate.setHashValueSerializer(valueSerializer);
+        return redisTemplate;
+    }
+}

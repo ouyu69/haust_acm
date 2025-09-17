c6eb02baeb5d9ecaa7ece64a4862fc609d47ca49
ouyu69 <zhiyuanyang410@gmail.com>
2025-09-17 19:02:25 +0800

    常量类

---
+++ src/main/java/com/haust_acm/entity/constants/Constants.java
@@ -0,0 +1,12 @@
+package com.haust_acm.entity.constants;
+
+/**
+ * @FileName Constants
+ * @Description
+ * @Author ouyu
+ * @Date 2025-09-16
+ **/
+public class Constants {
+    public static final String CHECK_CODE_KEY = "check_code_key";
+    public static final String CHECK_CODE_KEY_EMAIL = "check_code_key_email";
+}
